382b1894ec53694cc35f1323505eb06468692b9f
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-08-02 09:26:48 +0000

    fixing syntax error

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -22,7 +22,8 @@ echo -e "<html>
 #create symbolic link. if exists recreate it
 sudo unlink /data/web_static/current || true && sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 #give ownership
-sudo chown -hR ubuntu:ubuntu
+sudo chown -hR ubuntu:ubuntu /data
 hbnbstatic="server_name _;\n\tlocation /hbnb_static{\n\talias \/data\/web_static\/current\n}"
 # nginx conf: serving shakir.tech/hbnb_static from /data/web_static/current
 sudo sed -i "s/server_name _;/$hbnbstatic/" /etc/nginx/sites-enabled/default
+sudo service nginx reload
