84b7324a8f173bd439d1d8586c692e655be8cfd3
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-08-02 08:56:49 +0000

    preparing to deploy

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# prepring webservers for deployment
+sudo apt-get update
+sudo apt-get install -y nginx
+
+# start nginx server
+sudo service nginx start
+
+# creating directories
+mkdir /data
+mkdir /data/web_static
+mkdir /data/web_static/releases
+mkdir /data/web_static/shared
+mkdir /data/web_static/releases/test
+echo -e "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+#create symbolic link. if exists recreate it
+sudo unlink /data/web_static/current || true && sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+#give ownership
+sudo -R chown ubuntu /data/
+sudo -R chgrp ubuntu /data/
+hbnbstatic="server_name _;\n\tlocation /hbnb_static{\n\talias \/data\/web_static\/current\n}"
+# nginx conf: serving shakir.tech/hbnb_static from /data/web_static/current
+sudo sed -i "s/server_name _;/$hbnbstatic/" /etc/nginx/sites-enabled/default
