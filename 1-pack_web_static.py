8890eee8118b5635affbeb6cfee53331e7a0dfa8
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-09-07 19:52:14 +0000

    using linux commands to zip web_static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-""" compreses the files in web_static into .tgz archive """
-import os
-import tarfile
+# Import Fabric's API module
+""" generates archive from web_static folder """
+from fabric.api import *
 from datetime import datetime
+archive_name = "web_static_{}.tgz".format(datetime.now().strftime("%Y%m%d%H%M%S"))
 
 def do_pack():
-    """ packs the web_static folder into .tgz archive"""
-    n = datetime.now()
-    y, m, d = n.year, n.month, n.day
-    h, mn, s = n.hour, n.minute, n.second
-    output_path = "web_static_{}{:02d}{:02d}{:02d}{:02d}{:02d}.tgz".format(
-                   y,m,d,h,mn,s)
-    with tarfile.open(output_path, "w:gz") as tar:
-        for root, dirs, files in os.walk("web_static"):
-            for file in files:
-                tar.add(os.path.join(root, file), arcname=file)
-            for dir in dirs:
-                tar.add(os.path.join(root, dir), arcname=dir)
+    """
+    Generates a .tgz archive from the contents of the web_static folder
+    """
+    local("mkdir -p versions")
+    result = local("tar -cvzf versions/{} web_static".format(archive_name))
+    if result.succeeded:
+        return "versions/{}".format(archive_name)
+    else:
+        return None
