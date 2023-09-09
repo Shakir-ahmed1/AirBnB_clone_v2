9c1f8429a01f340ddc57f88f44b463daaabe024c
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-09-09 20:27:30 +0000

    documenting the function

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,6 +7,7 @@ env.hosts = ['3.85.54.219', '3.85.136.250']
 
 
 def do_deploy(archive_path):
+    """ deployes the given archive """
     if not exists(archive_path):
         return False
     t1 = put(archive_path, "/t{archive_path}mp/")
@@ -14,17 +15,21 @@ def do_deploy(archive_path):
         return False
     file_name = archive_path.split('/')[-1]
     folder_name = file_name.split('.')[-2]
-    t2 = run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(file_name, folder_name))
+    t2 = run(
+        "tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(file_name, folder_name))
     if t2.failed:
         return False
     t3 = run("rm /tmp/{}".format(file_name))
     if t3.failed:
         return False
     t4 = run(
-        "mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}".format(folder_name, folder_name))
+        "mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}".format(
+            folder_name,
+            folder_name))
     if t4.failed:
         return False
-    t5 = run("rm -rf /data/web_static/releases/{}/web_static".format(folder_name))
+    t5 = run(
+        "rm -rf /data/web_static/releases/{}/web_static".format(folder_name))
     if t5.failed:
         return False
     t6 = run("rm -rf /data/web_static/current")
