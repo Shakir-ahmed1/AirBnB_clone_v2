e549746f16f15898ddf5e838f45edd5b47779eb0
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-09-09 13:48:25 +0000

    extract tar and deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,36 +1,37 @@
 #!/usr/bin/python3
 """ a fabric script that deploys the current version of web_static """
 from fabric.api import *
-
+from os.path import exists
 
 env.hosts = ['', '']
+
+
 def do_deploy(archive_path):
-	if not exists(archive_path):
-		return False
-	t1 = put(archive_path, "/tmp/")
-	if t1.failed:
-		return False
-	file_name = archive_path.split('/')[-1]
-	folder_name = file_name.split('.')[-2]
-	t2 = run(f"tar -xzf /tmp/{file_name} -C "
-		  "/data/web_static/releases/{folder_name}/")
-	if t2.failed:
-		return False
-	t3 = run("rm /tmp/{file_name}")
-	if t3.failed:
-		return False
-	t4 = run("mv /data/web_static/releases/{folder_name}/web_static/*"
-		 "/data/web_static/releases/{folder_name}/")
-	if t4.failed:
-		return False
-	t5 = run("rm -rf /data/web_static/releases/{folder_name}/web_static/")
-	if t5.failed:
-		return False
-	t6 = run("rm -rf /data/web_static/current")
-	if t6.failed:
-		return False
-	t7 = run("ln -s /data/web_static/releases/{folder_name}"
-		 "/data/web_static/current")
-	if t7.failed:
-		return False
-	return True
+    if not exists(archive_path):
+        return False
+    t1 = put(archive_path, "/tmp/")
+    if t1.failed:
+        return False
+    file_name = archive_path.split('/')[-1]
+    folder_name = file_name.split('.')[-2]
+    t2 = run(f"tar -xzf /tmp/{file_name} -C /data/web_static/releases/{folder_name}/")
+    if t2.failed:
+        return False
+    t3 = run("rm /tmp/{file_name}")
+    if t3.failed:
+        return False
+    t4 = run(
+        "mv /data/web_static/releases/{folder_name}/web_static/* /data/web_static/releases/{folder_name}/")
+    if t4.failed:
+        return False
+    t5 = run("rm -rf /data/web_static/releases/{folder_name}/web_static/")
+    if t5.failed:
+        return False
+    t6 = run("rm -rf /data/web_static/current")
+    if t6.failed:
+        return False
+    t7 = run(
+        "ln -s /data/web_static/releases/{folder_name} /data/web_static/current")
+    if t7.failed:
+        return False
+    return True
