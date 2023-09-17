f6bbed8efb5659b211a1ff8229d3e94196bb27b6
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-09-17 16:19:34 +0000

    filling the requirements 2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -30,4 +30,4 @@ sudo chown -hR ubuntu:ubuntu /data
 
 # Use alias inside your Nginx configuration
 sudo sed -i "38i \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
-sudo service nginx restart
+sudo service nginx reload
