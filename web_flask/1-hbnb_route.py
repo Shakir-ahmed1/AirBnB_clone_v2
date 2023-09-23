86aedce1b6ec20195b371334e67f3cd54ad4ae01
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-09-23 20:33:22 +0000

    matching requirements

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,6 +1,8 @@
 #!/user/bin/python3
 """ hello HBNB """
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
@@ -16,4 +18,5 @@ def hbnb():
     return "HBNB"
 
 
-app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run()
