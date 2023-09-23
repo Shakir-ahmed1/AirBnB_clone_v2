43f1726a94db023ed0aacc8a7c49fdb1ef1a5b91
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-09-23 09:35:58 +0000

    first commit

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/user/bin/python3
+""" hello HBNB """
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ displays hello HBNB! page """
+    return "Hello HBNB!"
+
+app.run(host='0.0.0.0')
