8d24208112453ac6eb36929a6aec0d52580d9e31
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-09-23 20:45:28 +0000

    debugging

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,15 @@
-#!/usr/bin/python3
-"""Flask framework
-    """
+#!/user/bin/python3
+""" hello HBNB """
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_world():
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ displays hello HBNB! page """
     return "Hello HBNB!"
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
