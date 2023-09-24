0337e7f03bf3154938a0c90ec2c49f679337224b
Shakir-ahmed1 <shakirahmedsalih35756@gmail.com>
2023-09-24 14:40:41 +0000

    setting default routes

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,30 @@
+#!/user/bin/python3
+""" hello HBNB """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ displays hello HBNB! page """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ displays HBNB page """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ displays the uri passed to the text argument """
+    return f"C {text.replace('_', ' ')}"
+
+@app.route('/python', defaults={'text' : 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """ displays the uri passed to the text argument with default value """
+    return f"Python {text.replace('_', ' ')}"
+
+app.run(host='0.0.0.0', port=5000)
